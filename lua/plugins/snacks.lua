91b54dfb8d59194745a62d3cd2c24e8a47500b93
Yurij Mikhalevich <yurij@mikhalevi.ch>
2026-05-31 08:53:45 +1000

    feat: enable image rendering

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,15 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    image = {
+      enabled = true,
+      doc = {
+        enabled = true,
+      },
+      math = {
+        enabled = true,
+      },
+    },
     picker = {
       -- show hidden and ignored files in the explorer 
       hidden = true,
